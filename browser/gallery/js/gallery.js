b90cd52385d8b85b68e2a9c4bf47b450782ac87a
Pavel Pavlovskiy <pavlovskiypavel1989@gmail.com>
2018-07-24 18:12:15 +0700

    Исправил код: написал отдельную функцию для клика по правой (flipOnNext) и левой кнопке (flipOnPrev), вынес из функций и сохранил в отдельных переменных повторяющиеся части кода, сократил часть записей кода.

--- browser/gallery/js/gallery.js
+++ browser/gallery/js/gallery.js
@@ -1,5 +1,7 @@
 'use strict';
-const photoGallery = document.getElementById('currentPhoto');
+const gallery = document.getElementById('currentPhoto');
+const nextButton = document.getElementById('nextPhoto');
+const prevButton = document.getElementById('prevPhoto');
 const gallerySRCs = [
   'https://netology-code.github.io/hj-homeworks/browser/gallery/i/breuer-building.jpg', 
   'https://netology-code.github.io/hj-homeworks/browser/gallery/i/guggenheim-museum.jpg', 
@@ -7,37 +9,27 @@ const gallerySRCs = [
   'https://netology-code.github.io/hj-homeworks/browser/gallery/i/IAC.jpg', 
   'https://netology-code.github.io/hj-homeworks/browser/gallery/i/new-museum.jpg'
 ];
+const lastImageNum = gallerySRCs.length - 1;
 
-function flipOnNext(galleryImagesLinks, curntImageNum, lastImageNum) {
-  const gallery = document.getElementById('currentPhoto');
-  const nextImageNum = curntImageNum + 1;
-  (curntImageNum !== lastImageNum) ? gallery.src = galleryImagesLinks[nextImageNum] : gallery.src = galleryImagesLinks[0];
-}
 
-function flipOnPrev(galleryImagesLinks, curntImageNum, lastImageNum) {
-  const gallery = document.getElementById('currentPhoto');
-  const prevImageNum = curntImageNum - 1;
-  (curntImageNum !== 0) ? gallery.src = galleryImagesLinks[prevImageNum] : gallery.src = galleryImagesLinks[lastImageNum];
-}
-
-function clickOnGalleryButton() {
-  const gallery = document.getElementById('currentPhoto');
-  const nextButton = document.getElementById('nextPhoto');
-  const prevButton = document.getElementById('prevPhoto');
-  const currentImageLink = gallery.src;
-  //const currentImageLink = gallery.src.replace(/http:|https:/, ''); -  this code is used for "gallerySRCs" links starting on "// ..." (without "http:" or "https:").
-  const currentImageNumber = gallerySRCs.indexOf(currentImageLink);
-  const lastImageNumber = gallerySRCs.length - 1;
-
-  if (gallerySRCs.includes(currentImageLink) && this === nextButton) {
-    flipOnNext(gallerySRCs, currentImageNumber, lastImageNumber);
-  } else if (gallerySRCs.includes(currentImageLink) && this === prevButton) {
-    flipOnPrev(gallerySRCs, currentImageNumber, lastImageNumber);
+function flipOnNext() {
+  if (gallerySRCs.includes(gallery.src)) {
+    const currentImageNum = gallerySRCs.indexOf(gallery.src);
+    let nextImageNum = currentImageNum + 1;
+    nextImageNum = (currentImageNum !== lastImageNum) ? nextImageNum : 0;
+    gallery.src = gallerySRCs[nextImageNum];
   }
 }
 
-photoGallery.src = gallerySRCs[0];
-for (const btnID of ['nextPhoto', 'prevPhoto']) {
-  const btn = document.getElementById(btnID);
-  btn.onclick = clickOnGalleryButton;
+function flipOnPrev() {
+  if (gallerySRCs.includes(gallery.src)) {
+    const currentImageNum = gallerySRCs.indexOf(gallery.src);
+    let prevImageNum = currentImageNum - 1;
+    prevImageNum = (currentImageNum !== 0) ? prevImageNum : lastImageNum;
+    gallery.src = gallerySRCs[prevImageNum]; 
+  }
 }
+
+gallery.src = gallerySRCs[0];
+nextButton.onclick = flipOnNext;
+prevButton.onclick = flipOnPrev;
