1d3460dff2ffbfe79a7a59fef10655533babdf7a
Pavel Pavlovskiy <pavlovskiypavel1989@gmail.com>
2018-07-25 19:17:48 +0700

    Вывел переменную currentImageNum в глобальное пространство чтобы функции flipOnPrev и flipOnNext обращались обе к ней

--- browser/gallery/js/gallery.js
+++ browser/gallery/js/gallery.js
@@ -9,27 +9,29 @@ const gallerySRCs = [
   'https://netology-code.github.io/hj-homeworks/browser/gallery/i/IAC.jpg', 
   'https://netology-code.github.io/hj-homeworks/browser/gallery/i/new-museum.jpg'
 ];
+gallery.src = gallerySRCs[0];
 const lastImageNum = gallerySRCs.length - 1;
+let currentImageNum = gallerySRCs.indexOf(gallery.src);
 
 
 function flipOnNext() {
   if (gallerySRCs.includes(gallery.src)) {
-    const currentImageNum = gallerySRCs.indexOf(gallery.src);
     let nextImageNum = currentImageNum + 1;
     nextImageNum = (currentImageNum !== lastImageNum) ? nextImageNum : 0;
     gallery.src = gallerySRCs[nextImageNum];
+    currentImageNum = nextImageNum;
+    
   }
 }
 
 function flipOnPrev() {
   if (gallerySRCs.includes(gallery.src)) {
-    const currentImageNum = gallerySRCs.indexOf(gallery.src);
     let prevImageNum = currentImageNum - 1;
     prevImageNum = (currentImageNum !== 0) ? prevImageNum : lastImageNum;
     gallery.src = gallerySRCs[prevImageNum]; 
+    currentImageNum = prevImageNum;
   }
 }
 
-gallery.src = gallerySRCs[0];
 nextButton.onclick = flipOnNext;
 prevButton.onclick = flipOnPrev;
