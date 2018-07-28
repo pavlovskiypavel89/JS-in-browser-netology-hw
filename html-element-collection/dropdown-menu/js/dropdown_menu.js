e254681ed5de1a97a9f3d47baa519b92fe0f7280
Pavel Pavlovskiy <pavlovskiypavel1989@gmail.com>
2018-07-28 14:12:56 +0700

    Убрал логику для события onclick для пунктов меню выпадающего списка т.к. работает и без нее

--- html-element-collection/dropdown-menu/js/dropdown_menu.js
+++ html-element-collection/dropdown-menu/js/dropdown_menu.js
@@ -1,12 +1,8 @@
 ﻿'use strict';
 const dropdownButton = document.getElementsByClassName('wrapper-dropdown')[0];
-const dropdownMenuItems = dropdownButton.getElementsByClassName('dropdown')[0].getElementsByTagName('li');
 
 function toggleActiveClass() {
   this.classList.toggle('active');
 }
 
 dropdownButton.onclick = toggleActiveClass;
-for (const menuItem of dropdownMenuItems) {
-  menuItem.onclick = toggleActiveClass;
-}
