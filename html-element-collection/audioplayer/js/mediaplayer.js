cd8574f9b7632f70e84b3ce4c9bbad3f17a9e293
Pavel Pavlovskiy <pavlovskiypavel1989@gmail.com>
2018-07-28 12:16:18 +0700

    Оптимизировал код, убрал лишние переменные, поправил кнопку play/pause чтобы была кликабельна вся область кнопки

--- html-element-collection/audioplayer/js/mediaplayer.js
+++ html-element-collection/audioplayer/js/mediaplayer.js
@@ -1,6 +1,11 @@
 ﻿'use strict';
 const mediaPlayer = document.getElementsByClassName('mediaplayer')[0];
 const player = mediaPlayer.getElementsByTagName('audio')[0];
+const mediaPlayerControlTitlePanel = mediaPlayer.getElementsByClassName('controls')[0].getElementsByClassName('title')[0];
+const playOrPauseBtn = mediaPlayer.getElementsByClassName('playstate')[0];
+const stopBtn = mediaPlayer.getElementsByClassName('stop')[0];
+const nextBtn = mediaPlayer.getElementsByClassName('next')[0];
+const backBtn = mediaPlayer.getElementsByClassName('back')[0];
 const trackList = [ 
 	{title: 'LA Chill Tour', src: 'https://netology-code.github.io/hj-homeworks/html-element-collection/audioplayer/mp3/LA%20Chill%20Tour.mp3'},
 	{title: 'This is it band', src: 'https://netology-code.github.io/hj-homeworks/html-element-collection/audioplayer/mp3/This%20is%20it%20band.mp3'},
@@ -9,24 +14,13 @@ const trackList = [
 player.src = trackList[0].src;
 const lastTrackNum = trackList.length - 1;
 let currentTrackNum = trackList.findIndex(track => (track.src === player.src));
-const mediaPlayerControlTitlePanel = mediaPlayer.getElementsByClassName('controls')[0].getElementsByClassName('title')[0];
-const mediaPlayerControlBtns = mediaPlayer.getElementsByClassName('controls')[0].getElementsByClassName('buttons')[0];
-const playBtn = mediaPlayerControlBtns.getElementsByClassName('playstate')[0].getElementsByClassName('fa-play')[0];
-const pauseBtn = mediaPlayerControlBtns.getElementsByClassName('playstate')[0].getElementsByClassName('fa-pause')[0];
-const stopBtn = mediaPlayerControlBtns.getElementsByClassName('stop')[0];
-const nextBtn = mediaPlayerControlBtns.getElementsByClassName('next')[0];
-const backBtn = mediaPlayerControlBtns.getElementsByClassName('back')[0];
 
 function playCurrentTrack() {
 	if (mediaPlayer.classList.contains('play')) {
-	  mediaPlayer.classList.toggle('play');
+	  player.pause();
+	} else {
+	  player.play();
 	}
-	player.play();
-	mediaPlayer.classList.toggle('play');
-}
-
-function pauseCurrentTrack() {
-	player.pause();
 	mediaPlayer.classList.toggle('play');
 }
 
@@ -38,29 +32,31 @@ function stopCurrentTrack() {
 	}
 }
 
-function playSelectedTrack(selectedTrackNum) {
-	player.src = trackList[selectedTrackNum].src;
-	playCurrentTrack();
-	mediaPlayerControlTitlePanel.title = trackList[selectedTrackNum].title;
+function selectTrack(selectTrackNum) {
+	player.src = trackList[selectTrackNum].src;
+	mediaPlayerControlTitlePanel.title = trackList[selectTrackNum].title;
+	if (mediaPlayer.classList.contains('play')) { 
+		mediaPlayer.classList.toggle('play');
+		playCurrentTrack();
+	}
 }
 
 function flipOnNextTrack() {
 	let nextTrackNum = currentTrackNum + 1;
 	nextTrackNum = (currentTrackNum !== lastTrackNum) ? nextTrackNum : 0;
-	playSelectedTrack(nextTrackNum);
+	selectTrack(nextTrackNum);
 	currentTrackNum = nextTrackNum;
 }
 
 function flipOnBackTrack() {
 	let backTrackNum = currentTrackNum - 1;
 	backTrackNum = (currentTrackNum !== 0) ? backTrackNum : lastTrackNum;
-	playSelectedTrack(backTrackNum);
+	selectTrack(backTrackNum);
 	currentTrackNum = backTrackNum;
 }
 
 
-playBtn.onclick = playCurrentTrack;
-pauseBtn.onclick = pauseCurrentTrack;
+playOrPauseBtn.onclick = playCurrentTrack;
 stopBtn.onclick = stopCurrentTrack;
 nextBtn.onclick = flipOnNextTrack;
 backBtn.onclick = flipOnBackTrack;
