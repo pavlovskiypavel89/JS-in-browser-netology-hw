b2e4c1d2cc0fb1fd08466749e7d389ffa5c2debb
Pavel Pavlovskiy <40686677+pavlovskiypavel89@users.noreply.github.com>
2018-09-23 02:36:07 +0700

    Update counter.js

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,14 @@
-'use strict';
+'use srict';
+const counter = document.querySelector('.counter');
+const errsOutput = document.querySelector('output.errors');
+const wssURL = 'wss://neto-api.herokuapp.com/counter';
+const socket = new WebSocket(wssURL);
+
+socket.addEventListener('message', event => outputData());
+window.addEventListener('beforeunload', event => socket.close(1000));
+
+function outputData() {
+  const data = JSON.parse(event.data);
+  counter.textContent = data.connections;
+  errsOutput.textContent = data.errors;
+}
