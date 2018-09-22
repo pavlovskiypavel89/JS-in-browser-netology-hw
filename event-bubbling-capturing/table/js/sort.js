b1b647403bff96fd36f69d723b1d6c1869f1b50f
Pavel Pavlovskiy <40686677+pavlovskiypavel89@users.noreply.github.com>
2018-09-23 02:49:16 +0700

    Update sort.js

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,16 @@
-'use strict';
-
 function handleTableClick(event) {
-  
+  const propName = event.target.dataset.propName;
+  if (event.target.tagName === 'TH' && propName) {
+    let sortValue = event.target.dataset.dir;
+    sortValue = event.target.dataset.dir = !sortValue ? 1 : sortValue *= -1;
+    event.currentTarget.dataset.sortBy = propName;
+    sortTable(propName, sortValue);
+  }
 }
+
+/********************************
+ * Не менять код ниже           *
+ ********************************/
+
+const table = document.querySelector('table');
+table.addEventListener('click', handleTableClick);
