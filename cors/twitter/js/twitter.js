e085400359afbfa33c81f6245cddf2b84cc4d550
Pavel Pavlovskiy <40686677+pavlovskiypavel89@users.noreply.github.com>
2018-09-23 02:33:03 +0700

    Update twitter.js

--- cors/twitter/js/twitter.js
+++ cors/twitter/js/twitter.js
@@ -1,1 +1,26 @@
 'use strict';
+const jsonpURL = 'https://neto-api.herokuapp.com/twitter/jsonp';
+const container = document.querySelector('.container');
+const datasetNames = ['wallpaper', 'username', 'description', 'pic', 'tweets', 'followers', 'following'];
+const tagsWithDataset = {};
+datasetNames.forEach(name => tagsWithDataset[name] = container.querySelector(`[data-${name}]`));
+
+function parseData(obj) {
+  for (const key in obj) {
+    (key === 'pic' || key === 'wallpaper') ? (tagsWithDataset[key].src = obj[key]) : (tagsWithDataset[key].textContent = obj[key]);
+  }
+}
+
+function loadData(url) {
+  const cbName = 'cb' + String(Math.random()).slice(-5);
+  return new Promise((done, fail) => {
+    window[cbName] = done;
+
+    const script = document.createElement('script');
+    script.src = `${url}?callback=${cbName}`;
+    document.body.appendChild(script);
+    document.body.removeChild(script);
+  });
+}
+
+loadData(jsonpURL).then(parseData); 
