f5b8282e80b9e04e06c16703a2b7a3c810aca5b4
Pavel Pavlovskiy <40686677+pavlovskiypavel89@users.noreply.github.com>
2018-09-23 02:48:10 +0700

    Update cart.js

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,13 @@
 'use strict';
+const itemList = document.querySelector('main.items-list');
+function addItemToCart() {
+  if (event.target.classList.contains('add-to-cart')) {
+    event.preventDefault();
+    const item = {
+      'title': event.target.dataset.title,
+      'price': event.target.dataset.price
+    }
+    addToCart(item);
+  }
+}
+itemList.addEventListener('click', event => addItemToCart());
