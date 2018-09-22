6f3784e6f98feb7fa14449e4f6d3d565d5aa4672
Pavel Pavlovskiy <40686677+pavlovskiypavel89@users.noreply.github.com>
2018-09-23 02:34:31 +0700

    Update click.js

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,5 @@
 'use strict';
+const socket = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+socket.addEventListener('open', event => showBubbles(event.currentTarget));
+window.addEventListener('click', event => socket.send(JSON.stringify( {x: event.clientX, y: event.clientY} )));
