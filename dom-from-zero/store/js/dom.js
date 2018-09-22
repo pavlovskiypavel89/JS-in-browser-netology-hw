83e332140b3953684c2d24ce591f2299e29f9d99
Pavel Pavlovskiy <40686677+pavlovskiypavel89@users.noreply.github.com>
2018-09-23 02:51:44 +0700

    Update dom.js

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,22 @@
 'use strict';
+function createElement(node) {
+  if (typeof node === 'string') {
+    return document.createTextNode(node);
+  }
+  
+  const el = document.createElement(node.name || 'div');
+  if (node.props && typeof node.props === 'object') {
+    Object.keys(node.props).forEach(key => el.setAttribute(key, node.props[key]));
+  }
+  if (Array.isArray(node.childs)) {
+    el.appendChild(node.childs.reduce((fragment, child) => {
+        fragment.appendChild(createElement(child));
+        return fragment;
+      }, document.createDocumentFragment())
+    );
+  } else if ((typeof node.childs === 'string') || (typeof node.childs === 'number')) {
+    el.appendChild(document.createTextNode(node.childs)); 
+  }
+         
+  return el;
+}
