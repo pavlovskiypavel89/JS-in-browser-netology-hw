760f796cb7c068d0a9d66f1fd22012d0911cbfa9
Pavel Pavlovskiy <40686677+pavlovskiypavel89@users.noreply.github.com>
2018-09-23 02:44:31 +0700

    Update counter.js

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,18 @@
 'use strict';
+const counter = document.querySelector('#counter');
+const btns = document.querySelector('.wrap-btns');
+let value = localStorage.counter;
+counter.textContent = value = value ? value : 0;
+
+function btnClick() {
+  if (event.target.id === 'increment') {
+    ++value;
+  } else if (event.target.id === 'decrement') {
+    value = value > 0 ? (--value) : 0;    
+  } else if (event.target.id === 'reset') {
+    value = 0;
+  }
+  counter.textContent = localStorage.counter = value;
+}
+
+btns.addEventListener('click', event => btnClick());
