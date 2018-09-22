aaabd2ae150f62299ab574407f76ae913c237688
Pavel Pavlovskiy <40686677+pavlovskiypavel89@users.noreply.github.com>
2018-09-23 02:38:26 +0700

    Update socket.js

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,11 @@
 'use strict';
+const socket = new WebSocket('wss://neto-api.herokuapp.com/draw');
+socket.addEventListener('open', () => {
+  window.editor.addEventListener('update', (event) => event.canvas.toBlob(
+    blob => socket.send(blob), 'image/webp', 0.9)
+  );
+});
+
+socket.addEventListener('close', event => console.log(event.wasClean ? 'There was a clean closing' : `There was a disconnect. Reason: ${event.reason}`));
+window.addEventListener('beforeunload', () => socket.close(1000, 'Session normally closed'));
+socket.addEventListener('error', error => console.log(`Error: ${error.message}`));
